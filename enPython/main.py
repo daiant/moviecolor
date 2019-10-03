79ef51c52194ae34eed47c4df152b1f3f0bc9860
Daiant <cmg2512@gmail.com>
2019-10-03 16:44:22 +0200

    añadido demo y todo

--- enPython/main.py
+++ enPython/main.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageDraw
 import sys, os
 from os import listdir
 from os.path import isfile, join
+from decimal import Decimal
 def getFiles(dir):
 	path = os.path.abspath(dir)
 	list = sorted(os.listdir(dir))
@@ -16,6 +17,7 @@ def getAverage(dir):
 	#print(list)
 	index = 0;
 	rgb_list = []
+	print("Reading thumbnails...")
 	for file in list:
 		im = Image.open(file)
 
@@ -28,26 +30,33 @@ def getAverage(dir):
 				a,u,x = rgb_im.getpixel((w,h))
 				r += a
 				g += u
-				b += x 
+				b += x
 				count+=1
 		r = int(r/count)
 		g = int(g/count)
 		b = int(b/count)
 		rgb_list.append([[index],[r,g,b]])
 		index+=1
-		print(rgb_list)
+		percent = Decimal((index*100.0)/len(list))
+		print("{}% read".format(round(percent,2)))
 	print("Done reading")
 	return rgb_list
-
-def writeImg(list):
-	img = Image.new("RGB", (500,500))
+def writeImg(list, outfile):
+	w, h = int(len(list)/1.77), len(list)
+	frame = h/len(list)
+	img = Image.new("RGB", (w,h))
 	draw = ImageDraw.Draw(img)
-	draw.rectangle((0,0,100,100),fill=(155,150,27))
+	print("Started drawing...")
+	for i in range(len(list)) :
+		r,g,b = list[i][1][0],list[i][1][1],list[i][1][2]
+		draw.rectangle((0,i,w,i+1),fill=(r,g,b))
+
 	img.show()
-	img.save("test.jpg")
+	img.save(outfile)
+	print("Saved in {}".format(outfile))
 def main():
 	list = getAverage(sys.argv[1])
-	writeImg(list)
+	writeImg(list, os.path.abspath(sys.argv[2]))
 
 
 main()
